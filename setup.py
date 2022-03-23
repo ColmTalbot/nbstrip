86ea1dbe23c280f258f45d8191e7f093eb9ef7d3
Colm Talbot <colm.talbot@ligo.org>
2022-03-23 10:43:42 -0400

    fix url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='nbstrip',
     author="Colm Talbot",
-    url="github.com/ColmTalbot/nbstrip",
+    url="https://github.com/ColmTalbot/nbstrip",
     version='0.1.0',
     py_modules=['nbstrip'],
     entry_points='''
