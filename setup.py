577a67306a9ba36431bee65ab7ea443b26e397c2
Colm Talbot <colm.talbot@ligo.org>
2022-03-23 11:01:42 -0400

    move to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='nbstrip',
-    author="Colm Talbot",
-    url="https://github.com/ColmTalbot/nbstrip",
-    version='0.1.0',
-    py_modules=['nbstrip'],
-    entry_points='''
-        [console_scripts]
-        nbstrip=nbstrip:main
-    ''',
-)
+setup()
